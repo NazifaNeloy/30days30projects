b6eda862cbf72c6f04ce3b96aba7636a04c776c9
NazifaNeloy <241569685+NazifaNeloy@users.noreply.github.com>
2026-03-03 16:53:58 +0600

    added bookdata

---
+++ Book_Recommendation/script.js
@@ -0,0 +1,79 @@
+const library = [
+    { 
+        id: 1, 
+        title: "The Girls Who Disappeared", 
+        author: "Claire Douglas", 
+        link: "https://www.penguin.co.uk/books/446655/the-girls-who-disappeared-by-douglas-claire/9781405951180",
+        image: "./books/the girls who disappeared.jpg",
+        innerImage: "", 
+        tags: ["Mystery", "Dual Timelines", "Thriller"], 
+        genre: "Thriller",
+        description: "In a rural town, three girls drive into the woods. Only one returns. Twenty years later, a journalist tries to uncover the truth."
+    },
+    { 
+        id: 2, 
+        title: "Story of My Life", 
+        author: "Lucy Score", 
+        link: "https://www.barnesandnoble.com/w/story-of-my-life-lucy-score/1143431343",
+        image: "./books/story of my life.jpg",
+        innerImage: "",
+        tags: ["Small Town", "Second Chance", "Romance"], 
+        genre: "Romance",
+        description: "A small-town romance where old flames reconnect and must face their past."
+    },
+    { 
+        id: 3, 
+        title: "Icebreaker", 
+        author: "Hannah Grace", 
+        link: "https://www.simonandschuster.com/books/Icebreaker/Hannah-Grace/9781668026038",
+        image: "./books/ice breaker.jpg",
+        innerImage: "",
+        tags: ["Hockey", "Sports Romance", "Grumpy x Sunshine"], 
+        genre: "Sports",
+        description: "A figure skater and hockey captain are forced to share a rink, and sparks fly."
+    },
+    { 
+        id: 4, 
+        title: "Things We Never Got Over", 
+        author: "Lucy Score", 
+        link: "https://www.barnesandnoble.com/w/things-we-never-got-over-lucy-score/1139747850",
+        image: "./books/things we never got over.jpg",
+        innerImage: "",
+        tags: ["Small Town", "Grumpy x Sunshine", "Romance"], 
+        genre: "Romance",
+        description: "A runaway bride ends up in a small town with a grumpy barber."
+    },
+    { 
+        id: 5, 
+        title: "I Knew You Were Trouble", 
+        author: "Alicia Garnier", 
+        link: "https://www.goodreads.com/",
+        image: "./books/i knew u were trouble.jpg",
+        innerImage: "",
+        tags: ["Enemies to Lovers", "Drama", "Romance"], 
+        genre: "Romance",
+        description: "A dramatic romance filled with tension and attraction."
+    },
+    { 
+        id: 6, 
+        title: "The Love Hypothesis", 
+        author: "Ali Hazelwood", 
+        link: "https://www.penguinrandomhouse.com/books/673978/the-love-hypothesis-by-ali-hazelwood/",
+        image: "./books/the love hypothesis.jpg",
+        innerImage: "",
+        tags: ["Fake Dating", "STEM", "Romance"], 
+        genre: "Romance",
+        description: "A fake dating experiment between a PhD student and a professor."
+    },
+    { 
+        id: 7, 
+        title: "Love on the Brain", 
+        author: "Ali Hazelwood", 
+        link: "https://www.penguinrandomhouse.com/books/679583/love-on-the-brain-by-ali-hazelwood/",
+        image: "./books/love on the brain.jpg",
+        innerImage: "",
+        tags: ["STEM", "Enemies to Lovers", "Romance"], 
+        genre: "Romance",
+        description: "A neuroscientist forced to work with her rival on a NASA project."
+    }
+];
